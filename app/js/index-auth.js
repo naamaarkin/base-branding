0caef7c0860e36f1318ef26ea09d2fabce883a1b
vjrj <vjrj@ourproject.org>
2020-02-18 22:07:21 +0100

    Fix drawer auth links in index

--- app/js/index-auth.js
+++ app/js/index-auth.js
@@ -4,29 +4,43 @@ const authCookieName = 'ALA-Auth';
 const loginClass = 'signedIn';
 const logoutClass = 'signedOut';
 
-if (document.location.host === settings.mainDomain || document.location.host === 'localhost:3333') {
-  if (settings.isDevel) console.log("We are in the main url, let's see if we are authenticated");
-  // As this page is plain html, we have to detect if with are authenticated via Cookies
-  // NOTE: For make this work you need ala.cookie.httpOnly to false in /data/cas/config/application.yml
-  // We should use another way to see if it's authenticated
+var mainDrawerLoginStatusInIndex = () => {
+  if (document.location.host === settings.mainDomain || document.location.host === 'localhost:3333') {
+    if (settings.isDevel) console.log("We are in the main url, let's see if we are authenticated");
+    // As this page is plain html, we have to detect if with are authenticated via Cookies
+    // NOTE: For make this work you need ala.cookie.httpOnly to false in /data/cas/config/application.yml
+    // We should use another way to see if it's authenticated
 
-  let authCookie = Cookies.get(authCookieName, { domain: settings.mainDomain, path: '/' });
-  var in30Minutes = 1/48;
+    let authCookie = Cookies.get(authCookieName, { domain: settings.mainDomain, path: '/' });
+    var in30Minutes = 1/48;
 
-  if (typeof authCookie === 'undefined' && settings.isDevel) {
-    console.log("We set a test cookie if we are in development");
-    Cookies.set(authCookieName, '/', { expires: in30Minutes });
+    if (typeof authCookie === 'undefined' && document.location.host === 'localhost:3333') {
+      console.log("We set a test cookie if we are in development");
+      Cookies.set(authCookieName, '/', { expires: in30Minutes });
+    }
+
+    if (typeof authCookie !== 'undefined') {
+      // https://github.com/AtlasOfLivingAustralia/ala-bootstrap3/blob/master/grails-app/taglib/au/org/ala/bootstrap3/HeaderFooterTagLib.groovy
+      if (settings.isDevel) console.log("Auth cookie present so logged in");
+      $("#drawer-nav-menu").removeClass("::loginStatus::").addClass("signedIn");
+    } else {
+      if (settings.isDevel) console.log("No auth cookie not present so not-logged in");
+      $("#drawer-nav-menu").removeClass("::loginStatus:").addClass("signedOut");
+    }
   }
-
-  if (typeof authCookie !== 'undefined') {
-    // https://github.com/AtlasOfLivingAustralia/ala-bootstrap3/blob/master/grails-app/taglib/au/org/ala/bootstrap3/HeaderFooterTagLib.groovy
-    if (settings.isDevel) console.log("Auth cookie present so logged in");
-    $("#drawer-nav-menu").removeClass("signedOut").addClass("signedIn");
-  } else {
-    if (settings.isDevel) console.log("No auth cookie not present so not-logged in");
-    $("#drawer-nav-menu").removeClass("signedIn").addClass("signedOut");
+  else {
+    if (settings.isDevel) console.log("We aren't in the main url");
   }
-}
-else {
-  if (settings.isDevel) console.log("We aren't in the main url");
-}
+};
+
+$(function() {
+  // wait til drawer elements are visible
+  var checkExist = setInterval(function() {
+    if (window.jQuery && $('#drawer-nav-menu').length) {
+      clearInterval(checkExist);
+      mainDrawerLoginStatusInIndex();
+    } else {
+      if (settings.isDevel) console.log("drawer not loaded");
+    }
+  }, 1000);
+});
