91002d9b74a3a8f6557da7a93abe0fb38409f3e4
vjrj <vjrj@ourproject.org>
2020-03-05 10:13:00 +0300

    Settings and title updated

--- app/js/settings.js
+++ app/js/settings.js
@@ -2,12 +2,12 @@ module.exports = {
   isDevel: true,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
   enabledLangs: ['en', 'sw'],
-  mainDomain: 'tanbif.ditnet.ac.tz', // used for cookies (without http/https)
-  mainLAUrl: 'https://tanbif.ditnet.ac.tz',
+  mainDomain: 'freshwater.sua.ac.tz', // used for cookies (without http/https)
+  mainLAUrl: 'https://freshwater.sua.ac.tz',
   baseFooterUrl: 'https://tanbif.ditnet.ac.tz/tz-ui-2020-freshwater',
   services: {
     collectory: { url: 'https://collections.tanbif.ditnet.ac.tz', title: 'Collections' },
-    biocache: { url: 'https://records.tanbif.ditnet.ac.tz', title: 'Occurrence records' },
+    biocache: { url: 'https://freshwater.sua.ac.tz', title: 'Occurrence records' },
     bie: { url: 'https://species.tanbif.ditnet.ac.tz', title: 'Species' },
     regions: { url: 'https://regions.tanbif.ditnet.ac.tz', title: 'Regions' },
     lists: { url: 'https://lists.tanbif.ditnet.ac.tz', title: 'Species List' },
